e0c660b54f51fa95fa0cb95bf7df24ba2c52143c
Kritika <Kritikasagar1999@gmail.com>
2020-11-12 17:34:46 +0530

    react app using unsplash api

---
+++ src/components/Heading.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = styled.header`
+  max-width: 70rem;
+  margin: 2rem auto;
+  text-align: center;
+`;
+
+const H1 = styled.h1`
+  font-family: 'Oswald', sans-serif;
+  margin-bottom: 1em;
+`;
+
+
+export const Heading = () => {
+
+  return (
+    <Header>
+      <H1>Unsplash</H1>
+      <p>The power of photography</p>
+      <p></p>
+      {/* <form>
+        <Input type="text" placeholder="Search photos" />
+        <Button>Search</Button>
+      </form> */}
+    </Header>
+  )
+}
